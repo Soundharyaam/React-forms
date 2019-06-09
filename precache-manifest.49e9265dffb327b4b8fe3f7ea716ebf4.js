11717d0f742b3205df65cfe958360e4461e8b75f
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 19:17:23 +0530

    Updates

---
+++ precache-manifest.49e9265dffb327b4b8fe3f7ea716ebf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b38b9cc432f960fd0bd3",
+    "url": "/React-forms/static/js/runtime~main.b38b9cc4.js"
+  },
+  {
+    "revision": "f7fd3933f850be26307f",
+    "url": "/React-forms/static/js/main.f7fd3933.chunk.js"
+  },
+  {
+    "revision": "4bea1ff899dad996351e",
+    "url": "/React-forms/static/js/1.4bea1ff8.chunk.js"
+  },
+  {
+    "revision": "f7fd3933f850be26307f",
+    "url": "/React-forms/static/css/main.c76e2571.chunk.css"
+  },
+  {
+    "revision": "fbc3c9f353f943a7edf2ab5e9f508dc2",
+    "url": "/React-forms/index.html"
+  }
+];
